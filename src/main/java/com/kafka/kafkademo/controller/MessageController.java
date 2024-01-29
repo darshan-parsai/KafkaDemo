74a855c4ba40e2cb0ac4a24f1804cff43f4e5c9e
darshan-parsai <darshanparsai@gmail.com>
2024-01-29 15:11:34 +0530

    initial commit with the produsers

---
+++ src/main/java/com/kafka/kafkademo/controller/MessageController.java
@@ -0,0 +1,26 @@
+package com.kafka.kafkademo.controller;
+
+import com.kafka.kafkademo.kafka.KafkaProducer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+
+    private final KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("message sent to the topic");
+    }
+}
