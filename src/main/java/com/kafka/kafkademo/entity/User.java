761519ea86d8fbc73e7a3f4f3174e3f39defc744
darshan-parsai <darshanparsai@gmail.com>
2024-01-31 12:01:52 +0530

    added consumer for the json type

--- src/main/java/com/kafka/kafkademo/entity/User.java
+++ src/main/java/com/kafka/kafkademo/entity/User.java
@@ -14,4 +14,13 @@ public class User {
     private int id;
     private String firstName;
     private String lastName;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
